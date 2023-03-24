81ec60e3b8dc8f7f5f9d9c63558a76c19f273917
anclark686 <anclark686@gmail.com>
2023-03-24 17:23:14 -0400

    changing functions c & d

--- function_c.py
+++ function_c.py
@@ -1,10 +1,9 @@
-def merge_lists(list_a, list_b):
+def merge_lists(list_1, list_2):
     """ Returns a new list which is
         a combination of list_a and list_b
         without any duplicate elements.
     """
-    pass
-
+    return list(set(list_1) | set(list_2))
 
 if __name__ == "__main__":
     print(merge_lists([1, 1, 2, 3], [3, 4, 5]))
--- function_d.py
+++ function_d.py
@@ -2,7 +2,8 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    return max(numbers)
+    highest = max(numbers)
+    return highest
 
 
 if __name__ == "__main__":
